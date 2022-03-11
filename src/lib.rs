352f6813445247dd332ce8045f8be9e594bc3d88
Ken C.Y. Leung <kenleung5e28@gmail.com>
2022-03-11 11:58:16 +0800

    finish get_args

---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+use crate::EntryType::*;
+use clap::{App, Arg};
+use regex::Regex;
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug, Eq, PartialEq)]
+enum EntryType {
+    Dir,
+    File,
+    Link,
+}
+
+#[derive(Debug)]
+pub struct Config {
+    paths: Vec<String>,
+    names: Vec<Regex>,
+    entry_types: Vec<EntryType>,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("findr")
+        .version("0.1.0")
+        .author("Ken C.Y. Leung <kenleung5e28@gmail.com>")
+        .about("Rust find")
+        .arg(
+            Arg::with_name("paths")
+            .value_name("PATH")
+            .help("Search Paths")
+            .multiple(true)
+            .default_value(".")
+        )
+        .arg(
+            Arg::with_name("names")
+            .short("n")
+            .long("name")
+            .value_name("NAME")
+            .help("Name")
+            .multiple(true)
+        )
+        .arg(
+            Arg::with_name("entry_types")
+            .short("t")
+            .long("type")
+            .value_name("TYPE")
+            .help("Entry type")
+            .multiple(true)
+            .possible_values(&["f", "d", "l"])
+        )
+        .get_matches();
+    
+    Ok(Config {
+        paths: matches.values_of_lossy("paths").unwrap(),
+        names: matches.values_of_lossy("names")
+            .unwrap_or(vec![])
+            .iter()
+            .map(|s| Regex::new(s.as_str())
+                .map_err(|_| format!("Invalid --name \"{}\"", s))
+            )
+            .collect::<Result<Vec<_>, _>>()?,
+        entry_types: matches.values_of_lossy("entry_types")
+            .unwrap_or(vec![])
+            .iter()
+            .map(|t| match t.as_str() {
+                "f" => File,
+                "d" => Dir,
+                "l" => Link,
+                _ => unreachable!(),
+            })
+            .collect::<Vec<_>>(),
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:?}", config);
+    Ok(())
+}
